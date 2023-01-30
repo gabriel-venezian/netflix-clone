3f01128e801caf41721bf0ebe8630b8513c76fff
Gabriel Venezian <gabriel.venezian@hotmail.com>
2023-01-29 23:16:51 -0300

    CSS Reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './assets/styles/global.scss';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
